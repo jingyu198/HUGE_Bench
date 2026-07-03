d22c02b7dfe029a8ed36e974be1a11bbec53746e
Jingyu Guo <guo27@student.unimelb.edu.au>
2026-07-03 15:12:23 +0800

    Update author affiliations

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -14,9 +14,9 @@ const authorRows = [
   ],
   [
     { name: 'Hanlue Zhang', marks: '3', link: 'https://www.linkedin.com/in/hanlue-zhang-5913a4248/' },
-    { name: 'Fengming Huang', marks: '4,5' },
+    { name: 'Fengming Huang', marks: '2' },
     { name: 'Yu Yao', marks: '5', link: 'https://a5507203.github.io/' },
-    { name: 'Tongliang Liu', marks: '3,5,†', link: 'https://tongliang-liu.github.io/' },
+    { name: 'Tongliang Liu', marks: '5,3,†', link: 'https://tongliang-liu.github.io/' },
     { name: 'Mingming Gong', marks: '1,3,†', link: 'https://mingming-gong.github.io/' },
   ],
 ]
